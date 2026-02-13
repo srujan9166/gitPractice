b0e1fbe5f72c54067a757f8b6f8900eaa139ae10
Srujan <srujanrs916@gmail.com>
2026-02-13 14:50:54 +0530

    added pattern program

---
+++ P1.java
@@ -0,0 +1,29 @@
+import java.util.*;
+interface I{
+    void dup(List<Integer> list);
+}
+class P1{
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(2);
+            Set<Integer> set = new HashSet<>();
+        I obj = (l)->{
+          for(Integer i : l){
+            set.add(i);
+          }
+        };
+        obj.dup(list);
+        System.out.println(set);
+        System.out.println("null");
+    
+
+        for(int i =1;i<=5 ; i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
